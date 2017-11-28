3e312415d20d1c226b98e0b008421b0d40f04406
Lucas Olmedo <lcsolmedosilva@gmail.com>
2017-11-28 21:53:17 -0200

    controls play/pause/stop - fix estructure folder

---
+++ js/video.js
@@ -0,0 +1,18 @@
+function getVideo() {
+    return document.getElementById("video");
+}
+
+function play() {
+    var video = getVideo();
+    video.play();
+}
+
+function pause() {
+    var video = getVideo();
+    video.pause();
+}
+
+function stop() {
+    var video = getVideo();
+    video.load();
+}
